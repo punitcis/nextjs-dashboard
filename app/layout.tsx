265978856155c71589e20a95564fa085cf4195d6
RakeshY121 <rakesh.k@cisinlabs.com>
2023-12-18 18:56:36 +0530

    Speed Page Insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export default function RootLayout({
   children,
 }: {
